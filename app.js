d7a4f818477ab2b744586501c49a3005dc6d4950
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-10 21:22:45 +0530

    change

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ import subscriptionRouter from "./routes/subscriptionRoutes.js";
 
 const app = express();
 
-app.use("/api/vi/auth", authRouter);
-app.use("/api/vi/users", userRouter);
-app.use("/api/vi/subscription", subscriptionRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Subscription Tracker API.");
