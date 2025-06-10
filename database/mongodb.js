cdb7f1cc18d5f557ace2b05205a5f4b62fce8ee4
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-10 23:11:03 +0530

    chabge

---
+++ database/mongodb.js
@@ -0,0 +1,18 @@
+const { default: mongoose } = require("mongoose");
+import { DB_URI, NODE_ENV } from "./../config/env";
+
+if (!DB_URI) {
+    throw new Error("Please define the MONGODB_URI environment variable inside .env.<development/production>.local");
+}
+
+const connectToDatabase = async () => {
+    try {
+await mongoose.connect(DB_URI);
+    } catch (error) {
+        console.log("Error connecting to database: ", error);
+
+        process.exit(1);
+    }
+};
+
+export default connectToDatabase;
