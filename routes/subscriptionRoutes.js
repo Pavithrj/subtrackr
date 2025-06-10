4f93ac0863103bad6bf54a6b6b5fa58fd7b2d891
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-10 21:22:57 +0530

    change

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -6,20 +6,32 @@ subscriptionRouter.get("/", (req, res) => {
     res.send({ title: "GET all subscriptions" });
 });
 
-// subscriptionRouter.get("/:id", (req, res) => {
-//     res.send({ title: "GET user details" });
-// });
+subscriptionRouter.get("/:id", (req, res) => {
+    res.send({ title: "GET subscription details" });
+});
 
-// subscriptionRouter.post("/", (req, res) => {
-//     res.send({ title: "CREATE new user" });
-// });
+subscriptionRouter.post("/", (req, res) => {
+    res.send({ title: "CREATE subscription" });
+});
 
-// subscriptionRouter.put("/:id", (req, res) => {
-//     res.send({ title: "UPDATE user" });
-// });
+subscriptionRouter.put("/:id", (req, res) => {
+    res.send({ title: "UPDATE subscription" });
+});
 
-// subscriptionRouter.delete("/:id", (req, res) => {
-//     res.send({ title: "DELETE user" });
-// });
+subscriptionRouter.delete("/:id", (req, res) => {
+    res.send({ title: "DELETE subscription" });
+});
+
+subscriptionRouter.get("/user/:id", (req, res) => {
+    res.send({ title: "GET all user subscription" });
+});
+
+subscriptionRouter.put("/:id/cancel", (req, res) => {
+    res.send({ title: "CANCEL subscription" });
+});
+
+subscriptionRouter.get("/upcoming-renewals", (req, res) => {
+    res.send({ title: "GET Upcoming Renewals" });
+});
 
 export default subscriptionRouter;
