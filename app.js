cbc7dacd864f2b1ed8b0c52c9a0bd7127ba9ba66
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-11 19:00:48 +0530

    change

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { PORT } from "./config/env.js";
+import cookieParser from "cookie-parser";
 import userRouter from "./routes/userRoutes.js";
 import authRouter from "./routes/authRoutes.js";
 import subscriptionRouter from "./routes/subscriptionRoutes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
+import { PORT } from "./config/env.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
