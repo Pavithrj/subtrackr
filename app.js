373152c466e38d5cb728763fafa726de69d08242
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-11 18:51:04 +0530

    change

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ import userRouter from "./routes/userRoutes.js";
 import authRouter from "./routes/authRoutes.js";
 import subscriptionRouter from "./routes/subscriptionRoutes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/errorMiddleware.js";
 
 const app = express();
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Subscription Tracker API.");
