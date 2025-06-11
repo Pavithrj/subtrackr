eefa905c92f9bfdde9350a95632b063c941f4e4f
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-11 09:12:59 +0530

    chabge

--- database/mongodb.js
+++ database/mongodb.js
@@ -7,10 +7,8 @@ if (!DB_URI) {
 
 const connectToDatabase = async () => {
     try {
-        // await mongoose.connect(DB_URI);
-        await mongoose.connect("mongodb+srv://subtrackerAdmin:S@feTr4ck_2025!@subtrackercluster.ykbr6dn.mongodb.net/")
-            .then(() => console.log('Connected to MongoDB'))
-            .catch((err) => console.error('MongoDB connection error:', err));
+        await mongoose.connect(DB_URI);
+        console.log("Connecting to database: ");
     } catch (error) {
         console.log("Error connecting to database: ", error);
 
