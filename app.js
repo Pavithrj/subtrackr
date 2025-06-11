ede0cddb11b44a6815c5a5bbddcd8f8d5de3220f
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-11 09:13:13 +0530

    chabge

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.get("/", (req, res) => {
     res.send("Welcome to the Subscription Tracker API.");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
 
-    connectToDatabase();
+    await connectToDatabase();
 });
 
 export default app;
