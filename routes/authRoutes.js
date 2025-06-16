823da281a5fdc3916997fd936a3a596138c495ae
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-16 19:38:34 +0530

    044

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,6 +7,6 @@ authRouter.post("/sign-up", signUp);
 
 authRouter.post("/sign-in", signIn);
 
-authRouter.post("/sign-up", signOut);
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
