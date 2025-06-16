292bb6e8bb43536003a3b58081c9b1fb6506c6e9
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-06-16 06:07:41 +0000

    024

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,7 +25,7 @@ export const signUp = async (req, res, next) => {
 
         const newUsers = await User.create([{ name, email, password: hashedPassword }], { session });
 
-        const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES });
+        const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
 
         await session.commitTransaction();
         session.endSession();
