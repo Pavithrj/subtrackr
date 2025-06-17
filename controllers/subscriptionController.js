34368117e74d8e2aa7981aa3398228d3ec2cf7ae
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-17 07:45:35 +0530

    070

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -2,7 +2,7 @@ import Subscription from "../models/subscriptionModel";
 
 export const createSubscription = async (req, res, next) => {
     try {
-        const subscription = await subscription.create({
+        const subscription = await Subscription.create({
             ...req.body,
             user: req.user._id
         });
