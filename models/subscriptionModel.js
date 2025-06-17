f390ddf95cbff253122a9582a7892f2021bb28b6
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-17 08:25:24 +0530

    077

--- models/subscriptionModel.js
+++ models/subscriptionModel.js
@@ -63,7 +63,7 @@ const subscriptionSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
-subscriptionSchema.pre("save", function (next) => {
+subscriptionSchema.pre("save", function (next) {
     if (!this.renewalDate) {
         const renewalPeriods = {
             daily: 1,
