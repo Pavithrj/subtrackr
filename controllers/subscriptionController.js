b41af92fa49e3c36d70d6ad806daa2ae98fe2497
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-17 07:42:22 +0530

    066

---
+++ controllers/subscriptionController.js
@@ -0,0 +1,14 @@
+import subscription from "../models/subscriptionModel";
+
+export const createSubscription = async (req, res, next) => {
+    try {
+        const subscription = await subscription.create({
+            ...req.body,
+            user: req.user._id
+        });
+
+        res.status(201).json({ success: true, data: subscription });
+    } catch (error) {
+        next(error);
+    }
+};
