dac5e75789ff7c3db35fa01c6fd688bc4b65a826
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-30 19:07:40 +0530

    173

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import authRouter from './routes/authRoutes.js';
 import subscriptionRouter from './routes/subscriptionRoutes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/errorMiddleware.js';
-import { PORT } from "./config/env.js";
+import { PORT } from './config/env.js';
 import arcjetMiddleware from "./middlewares/arcjetMiddleware.js";
 // import workflowRouter from "./routes/workFlowRoutes.js";
 
