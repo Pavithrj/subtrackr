5be41effea577345e5dacbf75e9013a6f50a59b1
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-30 19:06:37 +0530

    170

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import userRouter from './routes/userRoutes.js';
 import authRouter from './routes/authRoutes.js';
-import subscriptionRouter from "./routes/subscriptionRoutes.js";
+import subscriptionRouter from './routes/subscriptionRoutes.js';
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
 import { PORT } from "./config/env.js";
