d6756710454978d1543def749f8244905ea6b357
Pavithr Jain <pavithrjain19@gmail.com>
2025-07-27 11:12:33 +0530

    264

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,5 +1,5 @@
-import { mongoose } from "mongoose";
-import { DB_URI, NODE_ENV } from "./../config/env.js";
+import { mongoose } from 'mongoose';
+import { DB_URI, NODE_ENV } from './../config/env.js';
 
 if (!DB_URI) {
     throw new Error("Please define the MONGODB_URI environment variable inside .env.<development/production>.local");
